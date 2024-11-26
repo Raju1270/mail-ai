2fbaf549beb5f957f5875361bf42ce1a61b5fe4f
Raju <64759881+Raju1270@users.noreply.github.com>
2024-11-26 12:47:51 +0530

    clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 export default async function Home() {
   return (
     <>
-      <div className="">Home page</div>
+      <div className="">
+      <div className="space-x-4">
+                    <Button>
+                        <Link href="/mail">Get Started</Link>
+                    </Button>
+                    <UserButton />
+                    </div>
+                    </div>
     </>
   );
 }
